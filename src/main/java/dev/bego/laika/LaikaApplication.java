38d189e914ac1b434fdcd3ca8d9be41f52e1edf4
Bego Blanco <begoblanfer@gmail.com>
2024-09-10 09:46:47 +0200

    firsto commit

---
+++ src/main/java/dev/bego/laika/LaikaApplication.java
@@ -0,0 +1,13 @@
+package dev.bego.laika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaikaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/bego/laika/LaikaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bego.laika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
